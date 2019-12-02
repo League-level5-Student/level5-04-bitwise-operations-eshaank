a4188a755ee7877731a6fc115881970b54948bda
League Student <league@iMac-15.attlocal.net>
2019-12-01 17:26:23 -0800

    Started base64 thing
    asdf

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -7,19 +7,73 @@ public class BinaryPrinter {
 	//Don't be afraid to use the methods that are already complete to finish the others.
 	//Create a main method to test your methods.
 	
-	public void printByteBinary(byte b) {
-	
+	public static void main(String[] args) {
+		byte b = 8;
+		short s = 100;
+		int i = 1000;
+		long l = 10000000;
+		
+		printByteBinary(b);
+//		System.out.println();
+//		printShortBinary(s);
+//		System.out.println();
+//		printIntBinary(i);
+//		System.out.println();
+//		printLongBinary(l);
 	}
 	
-	public void printShortBinary(short s) {
-		
+	public static void printByteBinary(byte b) {
+		//StringBuilder num = new StringBuilder();
+		String num = "";
+		for (int i = 7; i >= 0; i--) {
+			byte numShift = (byte) (b >> i);
+			
+			if ((numShift & 1) > 0) {
+				num += "1";
+			}
+			else {
+				num += "0";
+			}
+		}
+		System.out.println(num);
 	}
 	
-	public void printIntBinary(int i) {
-		
+	public static void printShortBinary(short s) {
+		for (int i = 15; i >= 0; i--) {
+			short numShift = (short) (s >> i);
+			
+			if ((numShift & 1) > 0) {
+				System.out.print("1");
+			}
+			else {
+				System.out.print("0");
+			}
+		}
 	}
 	
-	public void printLongBinary(long l) {
-		
+	public static void printIntBinary(int i) {
+		for (int j = 31; j >= 0; j--) {
+			byte numShift = (byte) (i >> j);
+			
+			if ((numShift & 1) > 0) {
+				System.out.print("1");
+			}
+			else {
+				System.out.print("0");
+			}
+		}
+	}
+	
+	public static void printLongBinary(long l) {
+		for (int i = 63; i >= 0; i--) {
+			byte numShift = (byte) (l >> i);
+			
+			if ((numShift & 1) > 0) {
+				System.out.print("1");
+			}
+			else {
+				System.out.print("0");
+			}
+		}
 	}
 }
--- src/_04_Base64_Decoder/Base64Decoder.java
+++ src/_04_Base64_Decoder/Base64Decoder.java
@@ -5,51 +5,72 @@ import java.io.FileReader;
 
 public class Base64Decoder {
 	/*
-	 * Base 64 is a way of encoding binary data using text.
-	 * Each number 0-63 is mapped to a character. 
-	 * NOTE: THIS IS NOT THE SAME AS ASCII OR UNICODE ENCODING!
-	 * Since the numbers 0 through 63 can be represented using 6 bits, 
-	 * every four (4) characters will represent twenty four (24) bits of data.
-	 * 4 * 6 = 24
+	 * Base 64 is a way of encoding binary data using text. Each number 0-63 is
+	 * mapped to a character. NOTE: THIS IS NOT THE SAME AS ASCII OR UNICODE
+	 * ENCODING! Since the numbers 0 through 63 can be represented using 6 bits,
+	 * every four (4) characters will represent twenty four (24) bits of data. 4 * 6
+	 * = 24
 	 * 
-	 * For this exercise, we won't worry about what happens if the total bits being converted
-	 * do not divide evenly by 24.
+	 * For this exercise, we won't worry about what happens if the total bits being
+	 * converted do not divide evenly by 24.
 	 * 
 	 * If one char is 8 bits, is this an efficient way of storing binary data?
 	 * (hint: no)
 	 * 
-	 * It is, however, useful for things such as storing media data inside an HTML file (for web development),
-	 * so that way a web site does not have to look for an image, sound, library, or whatever in a separate location.
+	 * It is, however, useful for things such as storing media data inside an HTML
+	 * file (for web development), so that way a web site does not have to look for
+	 * an image, sound, library, or whatever in a separate location.
 	 * 
 	 * View this link for a full description of Base64 encoding
 	 * https://en.wikipedia.org/wiki/Base64
 	 */
-	
-	
-	final static char[] base64Chars = {
-		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
-		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
-		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
-	};
-	
-	//1. Complete this method so that it returns the the element in
-	//   the base64Chars array that corresponds to the passed in char.
-	public static byte convertBase64Char(char c){
+
+	final static char[] base64Chars = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+			'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+			'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5',
+			'6', '7', '8', '9', '+', '/' };
+
+	// 1. Complete this method so that it returns the the element in
+	// the base64Chars array that corresponds to the passed in char.
+	public static byte convertBase64Char(char c) {
+		byte element = 0;
+		for (int i = 0; i < base64Chars.length; i++) {
+			if (base64Chars[i] != c) {
+				element++;
+			} else {
+				return (byte) element;
+			}
+		}
+
 		return 0;
 	}
-	
-	//2. Complete this method so that it will take in a string that is 4 
-	//   characters long and return an array of 3 bytes (24 bits). The byte 
-	//   array should be the binary value of the encoded characters.
-	public static byte[] convert4CharsTo24Bits(String s){
+
+	// 2. Complete this method so that it will take in a string that is 4
+	// characters long and return an array of 3 bytes (24 bits). The byte
+	// array should be the binary value of the encoded characters.
+	public static byte[] convert4CharsTo24Bits(String s) {
+		String num = "";
+		
+		for (int i = 0; i < s.length(); i++) {
+			char letter = s.charAt(i);
+			for (int j = 7; j >= 0; j--) {
+				byte numShift = (byte) (letter >> j);
+
+				if ((numShift & 1) > 0) {
+					num += "1";
+				} else {
+					num += "0";
+				}
+			}
+		}
+		System.out.println(num);
 		return null;
 	}
-	
-	//3. Complete this method so that it takes in a string of any length
-	//   and returns the full byte array of the decoded base64 characters.
+
+	// 3. Complete this method so that it takes in a string of any length
+	// and returns the full byte array of the decoded base64 characters.
 	public static byte[] base64StringToByteArray(String file) {
+
 		return null;
 	}
 }
